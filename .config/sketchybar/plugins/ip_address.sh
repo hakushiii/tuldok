b09889e5b0faf03b241749757ce4ca3d6bf873ac
Jerome A <jeromealapad13@gmail.com>
2024-10-23 18:25:34 +0800

    fix paddings

--- .config/sketchybar/plugins/ip_address.sh
+++ .config/sketchybar/plugins/ip_address.sh
@@ -9,18 +9,22 @@ if [[ $IS_VPN != "" ]]; then
 	COLOR=0xffb2d8d8
 	ICON=$ICON_VPN
 	LABEL="VPN"
+	R_PADDING=10
 elif [[ $IP_ADDRESS != "" ]]; then
 	COLOR=0xffffb7c5
 	ICON=$ICON_WIFI
 	LABEL=$IP_ADDRESS
+	R_PADDING=10
 else
 	COLOR=0xffffffff
 	ICON=$ICON_WIFI_OFF
-	LABEL="N/A"
+	LABEL=""
+	R_PADDING=5
 fi
 
 sketchybar --set $NAME background.color=0xff3B4252 \
 	icon=$ICON \
 	label="$LABEL" \
 	icon.color=$COLOR \
-	label.color=$COLOR
+	label.color=$COLOR	\
+	label.padding_right=$R_PADDING
